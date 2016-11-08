746ee3c623a10d2616a6853a16bf600bbbe273ea
Joseph <jjohn226@emich.edu>
2016-11-08 11:18:31 -0500

    working on bt scanner

--- app/src/main/java/com/joseph/nxtcontroller/MainScreen.java
+++ app/src/main/java/com/joseph/nxtcontroller/MainScreen.java
@@ -5,24 +5,56 @@ import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.Set;
 
 public class MainScreen extends AppCompatActivity {
 
     BluetoothAdapter BA;
     Set<BluetoothDevice> pairedDevices;
+    Button connectBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
+        connectBtn = (Button)findViewById(R.id.vv_btnConnect);
         BA = BluetoothAdapter.getDefaultAdapter();
-        if (!BA.isEnabled()){
-            Toast.makeText(getApplicationContext(), "Turned on",Toast.LENGTH_LONG).show();
+        if (BA.isEnabled()){
+            Toast.makeText(getApplicationContext(), "Bluetooth is enabled, attempting to connect",Toast.LENGTH_LONG).show();
+            Intent turnOn = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(turnOn, 0);
         }
-        //Intent turnOn = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-        //startActivityForResult(turnOn, 0);
+        else {
+            Toast.makeText(getApplicationContext(), "Bluetooth is disabled, please turn on",Toast.LENGTH_LONG).show();
+            Intent turnOn = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(turnOn, 0);
+        }
+
+        connectBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                discover();
+            }
+        });
+
+
     }
+
+
+    public void discover(){
+
+    }
+
+    public void list(){
+
+    }
+
+
 }
